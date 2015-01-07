68b42d41a5bdaf404ef234e9b38bac6f087ceb39
theanswer42 <sagar.gokhale@gmail.com>
2015-01-06 21:48:30 -0800

    will only copy on changes

--- backup_notes.sh
+++ backup_notes.sh
@@ -13,11 +13,17 @@ ts=`date +"%Y_%m_%d"`
 archive_name=${TMPDIR}/notes_${ts}.tar.Z
 
 cd ~
-tar -cJvf ${archive_name} ${NOTESDIR}
+tar -cJvf ${archive_name} ${NOTESDIR} > /dev/null
 md5sum ${archive_name} > ${archive_name}.md5sum
 
-cp ${archive_name} ${archive_name}.md5sum ${BACKUPDIR}/
-
+last_backup_md5_file=`ls -c ${BACKUPDIR}/*.md5sum | head -1`
+last_backup_md5=`cat ${last_backup_md5_file} | cut -f1 -d" "`
+if grep ${last_backup_md5} ${archive_name}.md5sum > /dev/null; then
+    echo "No Changes since last backup."
+else
+    echo "New changes detected. Copying."
+    cp ${archive_name} ${archive_name}.md5sum ${BACKUPDIR}/
+fi
 rm ${TMPDIR}/*
 
 
