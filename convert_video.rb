7db94d1c02213c3ba212292ca092c6b0713c6132
theanswer42 <sagar.gokhale@gmail.com>
2015-01-16 23:43:09 -0800

    Almost ready to start testing

--- convert_video.rb
+++ convert_video.rb
@@ -5,50 +5,44 @@
 # if(video stream is encoded with x264) { copy }
 # else { add x264 with options }
 
-# What are the input video codecs i see?
-# ansi mpeg4 msmpeg4v3 none theora
-#
-# out of these, mpeg4 is straightforward
-# ansi - my mistake.. all text files are treated as "ansi" video streams
-# msmpeg4v3 (MP43) seems just another video codec
-# theora - should be no problemo
-# none - there's not a lot with this, so this is strange. lets see how this goes.
-VIDEO_STREAMS_TO_COPY = ["h264", "png", "mjpeg"]
-VIDEO_STREAMS_TO_ENCODE = ["mpeg4", "msmpeg4v3", "theora", "none"]
-X264_ENCODE_OPTIONS = "libx264 -preset slow -crf 18"
-def get_video_options(stream)
-  if VIDEO_STREAMS_TO_COPY.include?(stream[:codec_name])
+CODECS = {
+  :video => {
+    :copy => ["h264", "png", "mjpeg"],
+    :encode => ["mpeg4", "msmpeg4v3", "theora", "none"],
+    :options => "libx264 -preset slow -crf 18",
+  },
+  :audio => {
+    :copy => ["aac", "mp3"],
+    :encode => ["ac3", "dts", "flac", "vorbis"],
+    :options => "libfdk_aac --cutoff 15000 -vbr 5",
+  },
+  :subtitle => {
+    :copy => ["mov_text"],
+    :encode => ["subrip", "ass", "dvdsub", "xsub", "microdvd"],
+    :options => "mov_text",
+  }
+}
+
+def get_stream_options(stream)
+  codecs = CODECS[stream[:codec_type].to_sym]
+  if codecs[:copy].include?(stream[:codec_name])
     return "copy"
-  elsif VIDEO_STREAMS_TO_ENCODE.include?(stream[:codec_name])
-    return X264_ENCODE_OPTIONS
+  elsif codecs[:encode].include?(stream[:codec_name])
+    return codecs[:options]
+  else
+    return ""
   end
-  ""
 end
 
-def get_subtitle_options(stream)
-  "-map #{stream[:stream_specifier]} -c:#{stream[:stream_specifier]} copy"
-end
-
-# For audio, there are these streams:
-# aac -> copy
-# ac3 -> encode 
-# dts -> encode
-# flac -> encode
-# mp3 -> copy
-# vorbis -> encode
-AUDIO_STREAMS_TO_COPY = ["aac", "mp3"]
-AUDIO_STREAMS_TO_ENCODE = ["ac3", "dts", "flac", "vorbis"]
-AAC_ENCODE_OPTIONS = "libfdk_aac --cutoff 15000 -vbr 5"
-def get_audio_options(stream)
-  if AUDIO_STREAMS_TO_COPY.include?(stream[:codec_name])
-    return "copy"
-  elsif AUDIO_STREAMS_TO_ENCODE.include?(stream[:codec_name])
-    return AAC_ENCODE_OPTIONS
+def get_sub_extract_options(stream)
+  options = get_stream_options(stream)
+  if options != "copy"
+    options = "subrip"
   end
-  ""
 end
 
 filename = ARGV[0]
+option = ARGV[1]
 
 raise "Not a file: #{filename}" unless(File.file?(filename))
 
@@ -79,19 +73,59 @@ ffprobe_output.each do |line|
   end
 end
 
-puts streams.inspect
+# First, generate an mp4 file: 
+unless File.exists?("#{File.basename(filename)}.mp4")
+  options = [" -nostats -fix_sub_duration -i \"#{filename}\""]
+  stream_counts = {}
+  streams.each do |stream|
+    stream_options = get_stream_options(stream)
+    unless stream_options.empty?
+      options << "-map #{stream[:stream_specifier]} -c:#{stream[:stream_specifier]} #{stream_options}"
+      stream_counts[stream[:codec_type]] ||= 0
+      stream_counts[stream[:codec_type]] += 1
+    else
+      stream[:skipped] = true
+    end
+  end
+  options << "\"#{File.basename(filename)}\""
 
-options = ["-fix_sub_duration -i \"#{filename}\""]
-stream_counts = {}
-streams.each do |stream|
-  stream_options = self.send("get_#{stream[:codec_type]}_options", stream)
-  unless stream_options.empty?
-    options << "-map #{stream[:stream_specifier]} -c:#{stream[:stream_specifier]} #{stream_options}"
-    stream_counts[stream[:codec_type]] ||= 0
-    stream_counts[stream[:codec_type]] += 1
-  else
-    stream[:skipped] = true
+  # We can now run the encode
+  puts "ffmpeg #{options.join(' ')}"
+end
+
+# next up, generate an srt file.
+unless File.exists?("#{File.basename(filename)}.srt")
+  # Usually, extracting the first subtitle track should be enough. Maybe extract
+  # The first english subtitle track.
+  # But one of the videos picked for testing had a particularly problematic ass stream
+  # Even though it works in vlc, doing anything with it results in an empty srt or mov_text
+  # stream and also a weird second "chapters" sub stream shows up.
+  #
+  # To get around this problem, we'll use the following rules:
+  # First, try and find an english srt
+  # Then the first english
+  # finally, just pick the first
+  
+  extract_subs_options = [" -fix_sub_duration -i \"#{filename}\""]
+  subtitle_streams = streams.select {|stream| next unless stream[:codec_type] == 'subtitle' }
+  
+  stream_to_extract =
+    subtitle_streams.detect {|stream| stream[:codec_name] != "ass" && stream[:language] == "eng"} ||
+    subtitle_streams.detect {|stream| stream[:language] == "eng"} ||
+    subtitle_streams.first
+
+  if stream_to_extract
+    subs_options = get_sub_extract_options(stream)
+    if !subs_options.empty?
+      extract_subs_options << "-map #{stream[:stream_specifier]} -c:#{stream[:stream_specifier]} #{subs_options}"
+      extract_subs_options << "\"#{File.basename(filename)}.srt\""
+      puts "ffmpeg #{extract_subs_options.join(' ')}"
+    end
   end
 end
 
-puts options.join(" ")
+# Finally, generate a vtt file
+if File.exists?("#{File.basename(filename)}.srt") &&
+   !File.exists?("#{File.basename(filename)}.vtt")
+  puts "ffmpeg -i \"#{File.basename(filename)}.srt\" -map s:0 -c:s:0 webvtt \"#{File.basename(filename)}.vtt\""
+end
